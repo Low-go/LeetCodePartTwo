a88d06e8045805b8b1ccbb4a6afb3eb9dc2ecd10
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-03 14:27:04 -1000

    minor progress on numofislands

--- numberOfIslands.py
+++ numberOfIslands.py
@@ -2,15 +2,24 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
+        if not grid: #nothing inside
+            return 0
+        
+
         m = len(grid) # rows
         n = len(grid[0]) # columns
+        visit = set()
+        islands = 0
 
         for i in range (m):
             for j in range(n):
+                if grid [i][j] == "1":
 
-                element = grid[i][j]
+                    element = grid[i][j]
 
-                print(f"Element at ({i}, {j}) : {element}")
+                #print(f"Element at ({i}, {j}) : {element}") #okay so i can target each individual element
+        return islands
 
 
 
