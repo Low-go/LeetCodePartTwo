6d88725afbcfa92bf87513078c99d0ed43e5e23f
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-18 12:17:57 -1000

    coinChange dynamic solution. Hated this one

--- numberOfIslands.py
+++ numberOfIslands.py
@@ -1,5 +1,8 @@
 from typing import List
+import collections
 
+
+# not done, I do not underdstand this completely yet so yea I will research breadth search before continuing this
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
@@ -12,11 +15,25 @@ class Solution:
         visit = set()
         islands = 0
 
+        def bfs(i, j):
+            q = collections.deque()
+            visit.add((i, j)) # save as a tuple to kinda be like a single element
+            q.append((i, j))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+
+                for dr, dc in directions :
+                    r, c = row + dr , col +dc
+
         for i in range (m):
             for j in range(n):
-                if grid [i][j] == "1":
+                if grid [i][j] == "1" and (i, j) not in visit: # so if the element is land
+                    bfs(i, j) #breath first search
+                    islands += 1
 
-                    element = grid[i][j]
+                    #element = grid[i][j]
 
                 #print(f"Element at ({i}, {j}) : {element}") #okay so i can target each individual element
         return islands
