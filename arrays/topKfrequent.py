4cd4e2ba7d9d1f4cd4c0535c5b7b2f9f54a86478
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-31 13:09:56 -1000

    topKfrequent.py

---
+++ arrays/topKfrequent.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dict = {}
+        final = []
+        for i in nums:
+            if i in dict:
+                dict[i] += 1 #increment the number 
+            else:
+                dict[i] = 1 # has not appeared
+
+        for i in range(k):
+            highest_key = max(dict, key=dict.get)
+            final.append(highest_key)
+            dict[highest_key] = 0
+        
+        return final
+
+        
+
+
+
+solution = Solution()
+result = solution.topKFrequent([1,2], 2)
+print(result)
