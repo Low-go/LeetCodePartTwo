b126ad60ca1cc67bf5b3e914dbcf218cb6af1de9
Low-go <lorranalvesgaldino98@gmail.com>
2024-08-22 18:00:11 -1000

    finish my verison. Might look up another simpler version and copy it for learning

--- arrays/validSudoku.py
+++ arrays/validSudoku.py
@@ -3,7 +3,9 @@ from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        map = {j: set() for j in range(len(board[0]))} # hopefully this will take care of logic with the column
+        columnMap = {j: set() for j in range(len(board[0]))} # hopefully this will take care of logic with the column
+
+        squareMap = {(i//3, j//3): set() for i in range(9) for j in range(9)}
         
         for i in range(len(board)):
 
@@ -11,23 +13,21 @@ class Solution:
 
             for j in range(len(board[i])):
 
+                if board[i][j] == '.':
+                    continue
+
                 if board[i][j] in rowSet:
                     return False
+                rowSet.add(board[i][j])
                 
-                if (board[i][j] in map[j]):
+                if (board[i][j] in columnMap[j]):
                     return False
-                
-                elif (board[i][j] != '.'):
-                    rowSet.add(board[i][j])
-                    map[j].add(board[i][j])
+                columnMap[j].add(board[i][j])
 
-                
-
-
-                
-                
-
-
+                square_key = (i//3, j//3)
+                if board[i][j] in squareMap[square_key]:
+                    return False
+                squareMap[square_key].add(board[i][j])
 
         return True
 
@@ -36,7 +36,7 @@ class Solution:
 
 solution = Solution()
 result = solution.isValidSudoku([["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
+,["6",".","3","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
 ,["4",".",".","8",".","3",".",".","1"]
