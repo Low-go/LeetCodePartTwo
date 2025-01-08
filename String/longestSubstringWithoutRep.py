f119660f07f61b4a37754ac653bfeb1268477d37
Low-go <lorranalvesgaldino98@gmail.com>
2025-01-07 23:17:41 -1000

    solve longestSubstring using sliding window

--- String/longestSubstringWithoutRep.py
+++ String/longestSubstringWithoutRep.py
@@ -22,10 +22,30 @@ class Solution:
         
         if len(holder) > res:
             res = len(holder)
+
         # return 
         return res
 
+    # we are learning the sliding window today
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        charSet = set() # apparently they only allow unique values
 
+        # for a sliding window we need two pointers,
+        # the right pointer can literally be the for loop 
+        # as it moves its way right, easier to manage
+        l = 0 
+        res = 0 # final output
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l]) # remove the leftmost character
+                l += 1
+            charSet.add(s[r])
+
+            res = max(res, r - l + 1)
+        return res
+
+# my solution is not perfectly working as I would wish or hope 
 solution = Solution()
 result = solution.lengthOfLongestSubstring("pwwkew")
 print(result)
