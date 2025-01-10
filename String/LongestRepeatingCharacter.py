d0a7ec2e757a8bb16ea5a1d048b59ebb781455de
Low-go <lorranalvesgaldino98@gmail.com>
2025-01-09 22:03:42 -1000

    halfway through longest Repeating Character

---
+++ String/LongestRepeatingCharacter.py
@@ -0,0 +1,10 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        pass
+
+
+
+
+solution = Solution()
+result = solution.characterReplacement("pwwkew")
+print(result)
