942bd22261d4ad76a3821ed893e4bfca66916d8c
Low-go <lorranalvesgaldino98@gmail.com>
2025-01-28 22:00:58 -1000

    proper solution to problem 2

--- order/2.py
+++ order/2.py
@@ -29,7 +29,23 @@ class Solution:
         else:
             return  self.helper(node.next) + str(node.val)
 
+    
+    def addTwoNumbers2(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
 
+        carry = 0
+        while l1 or l2 : # while they are non null. It ok if one is but the other isnt
+            v1 = l1.val if l1 else 0 # this is such a cool syntax. If l1 is not null l1.val else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit 
+            val = v1 + v2 + carry
+
+            #remove carry
+            carry = val // 10
+            # we only want the ones place digit for our val
+            val %= 10
 
 '''
 The above solution was totally wrong because it returned an index when it was supposed to return a linked list
