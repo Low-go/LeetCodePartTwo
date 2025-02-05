6e8496d9f0e3bd3b86f93c29d20a509ea46813ff
Low-go <lorranalvesgaldino98@gmail.com>
2025-02-04 19:19:36 -1000

    online solution, one less time complexity

--- order/5.py
+++ order/5.py
@@ -16,9 +16,37 @@ class Solution:
                     res = store
         return res
 
-# Hahahaha I actually brute forced this I'm low key kinda happy
+    # Hahahaha I actually brute forced this I'm low key kinda happy. It has a time complexity of On^3
+
+    # lets see online how to do one without brute force solution
+    def longestPalindrome2(self, s: str) -> str:
+        res = ""
+        resLen = 0 # longest length initially set to 0
+
+        for i in range (len(s)):
+            # check odd length 
+            l, r = i, i # they should be initialized to center position
+
+            while l >= 0 and r < len(s) and s[l] == s[r]: # while we know this is a palindrome
+                if (r - l + 1) > resLen: # if this palindrome is bigger update resLen
+                    res = s[l: r+1] # saving the string inside from l positon to r position
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l: r+1] # saving the string inside from l positon to r position
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
+
 
 
 solution = Solution()
-result = solution.longestPalindrome("cbbd")
+result = solution.longestPalindrome2("cbbd")
 print(result)
