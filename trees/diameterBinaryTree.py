7a86b1b9488138edf71abe8c80e3db08d7628fe5
Low-go <lorranalvesgaldino98@gmail.com>
2025-04-06 20:12:49 -1000

    diameter of binary tree

--- trees/diameterBinaryTree.py
+++ trees/diameterBinaryTree.py
@@ -12,25 +12,43 @@ class Solution:
 
         res = 0
 
-        if root.left or root.right is not None:
-            res += 1
+        def recursion(node):
+            nonlocal res
 
-        def recursive():
-
-            if root is None:
+        
+            if not node:
                 return 0
+            
+            height1 = recursion(node.left)
+            height2 = recursion(node.right)
+            
 
-            return 1 + self.diameterOfBinaryTree(root.left) + self.diameterOfBinaryTree(root.right)
+            current_diamter = height1 + height2
+            res = max(res, current_diamter)
+
+
+            return 1 + max(height1, height2)
+        
+        recursion(root)
+
+        return res
+        
+
+
+    
+
+
+
         
         return res
 
 
-root0 = TreeNode(0)
+
 root1 = TreeNode(1)
 root1.left = TreeNode(2)
 root1.right = TreeNode(3)
 root1.left.left = TreeNode(4)
 root1.left.right = TreeNode(5)
 solution = Solution()
-result = solution.diameterOfBinaryTree(root0)
+result = solution.diameterOfBinaryTree(root1)
 print(result)
