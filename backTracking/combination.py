74e8b1b1260f1ced7b9d1891e06de8d5d6c9b8e8
Low-go <lorranalvesgaldino98@gmail.com>
2025-05-10 16:23:24 -1000

    online solution for backtracking

--- backTracking/combination.py
+++ backTracking/combination.py
@@ -22,6 +22,31 @@ class Solution:
         return res
 
 
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+
+        def dfs(i, cur, total):
+            # we acheived what we wanted
+            if total == target:
+                res.append(cur.copy())
+                return
+            # if our pointer exceeds length or total is larger than target
+            if i >= len(candidates) or total > target:
+                return
+
+            # one tree where the candidate is included
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+
+            # in this one we did not include it
+            cur.pop()
+            dfs(i + 1, cur, total)
+        
+        dfs(0, [], 0)
+        return res
+
+
 solution = Solution()
 result = solution.combinationSum([2,3,6,7],7)
 print(result)
