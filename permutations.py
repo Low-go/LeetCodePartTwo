3ebcdeab0786f97dda27fdd9e9d758544875d71f
Low-go <lorranalvesgaldino98@gmail.com>
2025-05-12 22:17:00 -1000

    finish permutation online solution.

--- permutations.py
+++ permutations.py
@@ -37,6 +37,8 @@ class Solution:
         for p in perms:
             for i in range(len(p) + 1):
                 p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
 
         return res
         
