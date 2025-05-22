b2fa09cd0a4523517c3c3e890ce9e9ec042c0207
Low-go <lorranalvesgaldino98@gmail.com>
2025-05-21 20:54:28 -1000

    container with the most water, time limit exceeded

--- twoPointers.py/containerWithMostWater.py
+++ twoPointers.py/containerWithMostWater.py
@@ -2,10 +2,42 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
+        
+        res = 0
+
+        l, r = 0, 1
+
+        while l <= len(height) - 1:
+
+
+            if r >= len(height):
+                l +=1
+                r = l +1
+
+                if r >= len(height):
+                    break
+                
+
+            if height[l] < height[r]:
+                hold = height[l]
+            elif height[r] < height[l]:
+                hold = height[r]
+            else:
+                hold = height[l]
+
+            temp = hold * (r - l)
+
+            if temp > res:
+                res = temp
+            
+            r += 1
+
+        
+        return res
+
 
 
 
 solution = Solution()
-result = solution.subarrayGCD()
+result = solution.maxArea([1,8,6,2,5,4,8,3,7])
 print(result)
