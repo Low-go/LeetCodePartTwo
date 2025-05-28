14c754f6cec71e908be85aa9cddebf72aa14836e
Low-go <lorranalvesgaldino98@gmail.com>
2025-05-27 17:49:25 -1000

    solved container with mostwater!!!

--- twoPointers.py/containerWithMostWater.py
+++ twoPointers.py/containerWithMostWater.py
@@ -34,10 +34,45 @@ class Solution:
 
         
         return res
+    
+
+    # I did two pointers wrong here, I just imitated a nested loop
+    def maxArea2(self, height: List[int]) -> int:
+
+        res = 0
+
+        l, r = 0, len(height) - 1
+
+        left = False
+
+        while l < r:
+
+            if height[l] < height[r]:
+                hold = height[l]
+                left = True
+            elif height[r] < height[l]:
+                hold = height[r]
+            else:
+                hold = height[l]
+                left = True
+
+
+            temp = hold * (r - l)
+
+            if temp > res:
+                res = temp
+            
+            if left:
+                l += 1
+                left = False
+            elif left == False:
+                r -= 1
+
+        return res
 
 
 
 
 solution = Solution()
-result = solution.maxArea([1,8,6,2,5,4,8,3,7])
+result = solution.maxArea2([1,8,6,2,5,4,8,3,7])
 print(result)
