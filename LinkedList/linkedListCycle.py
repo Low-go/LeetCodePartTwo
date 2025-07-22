5ab8177aaeb13f7f681db8096bb0828cb07622f7
Low-go <lorranalvesgaldino98@gmail.com>
2025-07-21 14:28:24 -1000

    really close but does not acount for linked lists with the same value

--- LinkedList/linkedListCycle.py
+++ LinkedList/linkedListCycle.py
@@ -1,13 +1,46 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pass
+        
+        pointer1 = head
+        pointer2 = head.next
+
+        while pointer1.val != pointer2.val:
+
+            if pointer2.next is None:
+                return False
+                
+            pointer2 = pointer2.next
+
+            if pointer2.next is None:
+                return False
+            
+            pointer2 = pointer2.next
+
+            pointer1 = pointer1.next
+
+        return True
 
 solution = Solution()
+
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = None  
+
+print(solution.hasCycle(node1))
 
     
