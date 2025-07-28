1a38b3ab579d47492ccd2ad63838238af4f5b18d
Low-go <lorranalvesgaldino98@gmail.com>
2025-07-27 19:43:47 -1000

    reorderList. I believe this solves it but storing these memory objects in an array causes a memory limit exceeded

---
+++ LinkedList/reorderList.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        store = []
+        dummy = head
+        store.append(dummy)
+        
+        while dummy.next is not None:
+            dummy = dummy.next
+            store.append(dummy)
+            
+        dummy2 = ListNode()
+        l, r = 0, len(store) - 1
+
+        left_Turn = True
+        while l <= r:
+            if left_Turn:
+
+                dummy2.next = store[l]
+                left_Turn = False
+                dummy2 = store[l]
+                l += 1
+            else:
+                dummy2.next = store[r]
+                left_Turn = True
+                dummy2 = store[r]
+                r -= 1
+
+        # for i in store:
+        #     print (i.val)
+
+
+
+
+
+# Create nodes
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+# Link nodes together: 1 -> 2 -> 3 -> 4 -> 5
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+head = node1
+
+solution = Solution()
+result = solution.reorderList(head)
